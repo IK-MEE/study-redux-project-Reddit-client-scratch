8963c6c3280c2c8ad719b1ee87913bf9c991fa1c
Nuttapat <ikewmee@gmail.com>
2025-11-24 07:54:05 +0700

    implement store and redditSlice.js

---
+++ src/store/redditSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsynceThunk } from '@reduxjs/toolkit';
+
+// Thunk แบบง่าย ดึงโพสจาก subreddit เดียว
+export const fetchPosts = createAsynceThunk(
+    'reddit/fetchPosts',
+    async (subreddit) => {
+        const res = await fetch('https://www.reddit.com/${subreddit}.json');
+        const json = await res.json();
+        return json.data.children.map( (c) => c.data );
+    }
+);
+
+const redditSlice = createSlice({
+    name: 'reddit',
+    initialState: {
+        posts: [],
+        isLoading: false,
+        error: false,
+        selectedSubreddit: 'r/pics',
+    },
+    reducers: {
+        setSelectedSubreddit(state, action) {
+            state.selectedSubreddit = action.payload;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPosts.pending, (state) => {
+                state.isLoading = true;
+                state.error = false;
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.posts = action.payload;
+            })
+            .addCase(fetchPosts.rejected, (state) => {
+                state.isLoading = false;
+                state.error = true;
+            })
+    },
+})
+
+export const { setSelectedSubreddit } = redditSlice.actions;
+export default redditSlice.reducer;
