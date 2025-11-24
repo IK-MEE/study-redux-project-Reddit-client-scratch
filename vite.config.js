459dfea24586a9b708efc9e9ecfc18be5d80f0aa
Nuttapat <ikewmee@gmail.com>
2025-11-24 12:10:16 +0700

    update proxy to bypass CORS

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/r': {
+        target: 'https://www.reddit.com',
+        changeOrigin: true,
+        secure: true,
+      },
+      '/subreddits': {
+        target: 'https://www.reddit.com',
+        changeOrigin: true,
+        secure: true,
+      },
+      '/search': {
+        target: 'https://www.reddit.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
