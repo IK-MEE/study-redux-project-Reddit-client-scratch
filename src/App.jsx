8b985040522add21936dcb7f69f3b347357122c6
Nuttapat <ikewmee@gmail.com>
2025-11-24 08:05:56 +0700

    import react-redux & create simple display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { fetchPosts } from './store/redditSlice'
 
 function App() {
+  const dispatch = useDispatch();
+  const { posts, isLoading, error, selectedSubreddit } = useSelector( (state) => state.reddit);
   const [count, setCount] = useState(0)
+
+  useEffect( () => {
+    dispatch(fetchPosts(selectedSubreddit));
+  }, [dispatch, selectedSubreddit]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Failed to load posts.</p>
 
   return (
     <>
+      <div>
+        <h1>Reddit Client (From Scratch)</h1>
+        <p>Subreddit: {selectedSubreddit}</p>
+        <ul>
+          {posts.map( (post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </StrictMode>
 )
